1dd0bcae087d19da49daf535288bfabc1c3e7587
hilotter <hi-sato@hitomedia.jp>
2017-07-07 17:13:58 +0900

    :sparkles: init

---
+++ lib/prpr/mention_reviewers.rb
@@ -0,0 +1,7 @@
+require "prpr/mention_reviewers/version"
+
+module Prpr
+  module MentionReviewers
+    # Your code goes here...
+  end
+end
---
+++ lib/prpr/mention_reviewers/version.rb
@@ -0,0 +1,5 @@
+module Prpr
+  module MentionReviewers
+    VERSION = "0.1.0"
+  end
+end
